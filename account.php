d7d0e23c6ff72058bd565fd0e155eefbf22736de
Nicholas Boyce <32978779+nb0yc33@users.noreply.github.com>
2020-09-20 16:53:18 +1000

    Backend for gov table added

--- account.php
+++ account.php
@@ -26,6 +26,7 @@
     <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"
         integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
         crossorigin=""></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 
 		<?php include("header.php"); ?>
 </head>
@@ -41,25 +42,55 @@
                 <?php endif ?>
             </div>
             <div class="search-box">
-                <a href="#"><img class="search-img" src="images/search.png" alt=""></a>
+                <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-search" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" d="M10.442 10.442a1 1 0 0 1 1.415 0l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1 0-1.415z"/>
+                    <path fill-rule="evenodd" d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"/>
+                </svg>
                 <input type="text" name="search" id="search">
             </div>
         </div>
         <br>
         <div class="map" id="mapid"></div>
-        <div class="table">
-            <tr class="headings row">
-				<th id="heading">Latitude</th>
-				<th id="heading">Longitude</th>
-				<th id="heading">Ranking</th>
-				<th id="heading">Equipment</th>
-				<th id="heading">RecentTest</th>
-				<th id="heading">LastTest</th>
-				<th id="heading">Request</th>
-				<th id="heading">RequestDate</th>
-			    <th id="heading">WaterType</th>
-			</tr>
-        </div>
+        <table class="table table-condensed table-dark">
+            <tr>
+                <th scope="col">Latitude</th>
+				<th scope="col">Longitude</th>
+				<th scope="col">Ranking</th>
+				<th scope="col">Equipment</th>
+				<th scope="col">RecentTest</th>
+				<th scope="col">LastTest</th>
+				<th scope="col">Request</th>
+				<th scope="col">RequestDate</th>
+            </tr>
+            <?php
+                $conn = mysqli_connect("localhost", "gents", "truegents1", "water_quality");
+                // Check connection
+                if ($conn->connect_error) {
+                    die("Connection failed: " . $conn->connect_error);
+                }
+                $sql = "SELECT Latitude, Longitude, Ranking, Equipment, RecentTest, LastTest, Request, RequestDate FROM Sites";
+                $result = $conn->query($sql);
+                if ($result->num_rows > 0) {
+                // output data of each row
+                    while($row = $result->fetch_assoc()) {
+                        echo "<tr>
+                                <td>" . $row["Latitude"]. "</td>
+                                <td>" . $row["Longitude"] . "</td>
+                                <td>". $row["Ranking"]. "</td>
+                                <td>". $row["Equipment"]. "</td>
+                                <td>". $row["RecentTest"]. "</td>
+                                <td>". $row["LastTest"]. "</td>
+                                <td>". $row["Request"]. "</td>
+                                <td>". $row["RequestDate"]. "</td>
+                              </tr>";
+                    }
+                    echo "</table>";
+                } else { 
+                    echo "0 results"; 
+                }
+                $conn->close();
+            ?>
+        </table>
 	</div>
 	
 	<div class="waves">
